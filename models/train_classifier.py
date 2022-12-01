6e956c7144330ca8d5b81c0152c48d4804451889
prateek681 <85766211+prateek681@users.noreply.github.com>
2022-12-01 05:23:33 +0100

    Add Comments to train_classifier.py

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -18,7 +18,12 @@ from sklearn.multioutput import MultiOutputClassifier
 import pickle
 
 def load_data(database_filepath):
-    engine = create_engine('sqlite:///'+database_filepath)
+    '''
+    Load SQL Database and returns the Dataframes of Messages, columns and category names
+    Inputs: database_filepath: Filepath of where the Database is stored
+    Outputs: Dataframe files of Message, Columns and the category names
+    '''
+    engine = create_engine('sqlite:///'+ database_filepath)
     df = pd.read_sql_table('DisasterResponse', engine)
     X = df.message
     y = df[df.columns[4:]]
@@ -26,11 +31,20 @@ def load_data(database_filepath):
     return X, y, category_names 
 
 def tokenize(text):
+    '''
+    Tokenize the text
+    Inputs: Text
+    Outputs: clean tokens after tokenization
+    '''
+    # Character range for regex
+
     url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
     detected_urls = re.findall(url_regex, text)
+    
     for url in detected_urls:
         text = text.replace(url, "urlplaceholder")
 
+    # Initialize tokenizer
     tokens = word_tokenize(text)
     lemmatizer = WordNetLemmatizer()
 
@@ -43,12 +57,19 @@ def tokenize(text):
 
 
 def build_model():
+    '''
+    Build the Pipeline Model
+    Outputs: Pipeline Model file
+    '''
+
     pipeline = Pipeline([
         ('vect', CountVectorizer(tokenizer=tokenize)),
         ('tfidf', TfidfTransformer()),
         ('clf', MultiOutputClassifier(RandomForestClassifier()))
     ])
-    # Number of parameters are reduced significantly in order to accomodate Github upload option
+    
+    # Number of parameters are reduced significantly in order to make it possible to upload to Github
+
     parameters = {
         'clf__estimator__n_estimators': [10],
         'clf__estimator__min_samples_split': [2],
@@ -59,11 +80,22 @@ def build_model():
 
 
 def evaluate_model(model, X_test, y_test, category_names):
+    '''
+    Evaluate the Pipeline Model
+    Inputs: 
+    - model : The Pipeline Model
+    - X_test : Test Dataset
+    - y_test : Test Dataset
+    - category_names : Category Names 
+    '''
     y_pred = model.predict(X_test)
     class_report = classification_report(y_test, y_pred, target_names=category_names)
     print(class_report)
 
 def save_model(model, model_filepath):
+    '''
+    Save the Model as a pickle file
+    '''
     with open(model_filepath, 'wb') as file:
         pickle.dump(model, file)
 
